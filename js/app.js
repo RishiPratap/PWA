fe78a1c3ed78596849c218a1136b9512ee2ca1bb
Rishi Pratap <rp0258@srmist.edu.in>
2021-05-06 22:53:33 +0530

    add inde.html

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+  { name: "Perspiciatis", image: "images/coffee1.jpg", alt: "image1" },
+  { name: "Voluptatem", image: "images/coffee2.jpg", alt: "image1" },
+  { name: "Explicabo", image: "images/coffee3.jpg", alt: "image1" },
+  { name: "Rchitecto", image: "images/coffee4.jpg", alt: "image1" },
+  { name: " Beatae", image: "images/coffee5.jpg", alt: "image1" },
+  { name: " Vitae", image: "images/coffee6.jpg", alt: "image1" },
+  { name: "Inventore", image: "images/coffee7.jpg", alt: "image1" },
+  { name: "Veritatis", image: "images/coffee8.jpg", alt: "image1" },
+  { name: "Accusantium", image: "images/coffee9.jpg", alt: "image1" },
 ]
 
 const showCoffees = () => {
@@ -17,7 +17,7 @@ const showCoffees = () => {
       ({ name, image }) =>
         (output += `
                 <div class="card">
-                  <img class="card--avatar" src=${image} />
+                  <img class="card--avatar" src=${image} alt=${alt} />
                   <h1 class="card--title">${name}</h1>
                   <a class="card--link" href="#">Taste</a>
                 </div>
