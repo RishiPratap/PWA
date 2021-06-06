cf32cbc35caa0303e7e04a1700607693508a6374
Rishi Pratap <rp0258@srmist.edu.in>
2021-06-06 12:25:36 +0530

    install app

--- serviceWorker.js
+++ serviceWorker.js
@@ -23,6 +23,19 @@ self.addEventListener("install", installEvent => {
   )
 })
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(res => {
